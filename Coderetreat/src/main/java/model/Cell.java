d9cd62f351b42b3554404e9e60dec4762c11aa9a
karin <karin@DESKTOP-JFTOVL2>
2024-12-15 20:31:31 +0100

    last

---
+++ Coderetreat/src/main/java/model/Cell.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Cell {
+
+	private boolean state;
+	private int neighbours;
+
+	
+	public boolean getState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	public Cell(boolean state2, int neighbours) {
+		super();
+		this.state = state;
+		this.neighbours = neighbours;
+	}
+
+	public boolean iterate()
+	{
+		if(neighbours<2 || neighbours>3)
+		{
+			state=false;
+			return false;
+		}
+		else {
+			
+			state=true;
+			return true;
+		}
+
+	}
+	
+}
